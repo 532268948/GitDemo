8e60cb0cccabe5e71fe485654204427f5c38a3f2
532268948 <5322268948@qq.com>
2018-09-27 15:41:13 +0800

    11

--- app/src/main/java/com/zust/gitdemo/MainActivity.java
+++ app/src/main/java/com/zust/gitdemo/MainActivity.java
@@ -1,13 +1,36 @@
 package com.zust.gitdemo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.zust.gitdemo.bluetooth.BlueToothActivity;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button mButton1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mButton1 = findViewById(R.id.btn1);
+        mButton1.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        switch (id) {
+            case R.id.btn1:
+                startActivity(new Intent(this, BlueToothActivity.class));
+                break;
+            default:
+                break;
+        }
     }
 }
